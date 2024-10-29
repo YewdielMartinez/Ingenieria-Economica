8c37bc16ad8bfa060016bb14ca29fd27a6ee7dc0
YewdielMartinez <144299627+YewdielMartinez@users.noreply.github.com>
2024-10-29 11:49:23 -0700

    Se hicieron cambios en el menu agregando el modo oscuro

--- src/components/TIR.tsx
+++ src/components/TIR.tsx
@@ -1,10 +1,18 @@
-// src/components/TIR.tsx
-
 import React, { useState } from 'react';
-import styles from './SimpleInterest.module.css';
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  IconButton,
+  Box,
+} from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export const TIR: React.FC = () => {
-  const [cashFlows, setCashFlows] = useState<string>('');
+  const [initialInvestment, setInitialInvestment] = useState<number | string>('');
+  const [cashFlows, setCashFlows] = useState<number[]>([0]);
   const [irr, setIrr] = useState<number | null>(null);
 
   // Función para calcular el VPN dado una tasa de descuento
@@ -16,7 +24,7 @@ export const TIR: React.FC = () => {
 
   // Función para calcular la TIR usando búsqueda binaria
   const calculateIRR = () => {
-    const cashFlowArray = cashFlows.split(',').map(Number);
+    const cashFlowArray = [Number(initialInvestment), ...cashFlows];
     let lowerBound = 0;
     let upperBound = 1;
     let guessRate = (upperBound + lowerBound) / 2;
@@ -39,27 +47,82 @@ export const TIR: React.FC = () => {
 
   // Limpiar los resultados
   const clearInputs = () => {
-    setCashFlows('');
+    setInitialInvestment('');
+    setCashFlows([0]);
     setIrr(null);
   };
 
+  // Agregar un flujo de efectivo adicional
+  const addCashFlow = () => {
+    setCashFlows([...cashFlows, 0]);
+  };
+
+  // Manejar el cambio en cada flujo de efectivo
+  const handleCashFlowChange = (index: number, value: string) => {
+    const newCashFlows = [...cashFlows];
+    newCashFlows[index] = Number(value);
+    setCashFlows(newCashFlows);
+  };
+
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>Tasa Interna de Rendimiento (TIR)</h2>
-      <input
-        type="text"
-        placeholder="Flujos de efectivo separados por comas (e.g., -1000, 200, 300, 400)"
-        value={cashFlows}
-        onChange={(e) => setCashFlows(e.target.value)}
-        className={styles.input}
+    <Container sx={{ mt: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        Tasa Interna de Rendimiento (TIR)
+      </Typography>
+      <TextField
+        label="Inversión Inicial"
+        type="number"
+        value={initialInvestment}
+        onChange={(e) => setInitialInvestment(e.target.value)}
+        fullWidth
+        margin="normal"
       />
-      <button onClick={calculateIRR} className={styles.button}>
-        Calcular
-      </button>
-      <button onClick={clearInputs} className={styles.clearButton}>
-        Limpiar
-      </button>
-      {irr !== null && <p className={styles.result}>TIR: {irr.toFixed(2)}%</p>}
-    </div>
+
+      {cashFlows.map((cashFlow, index) => (
+        <Box display="flex" alignItems="center" key={index} sx={{ mt: 1 }}>
+          <TextField
+            label={`Flujo de Efectivo Año ${index + 1}`}
+            type="number"
+            value={cashFlow}
+            onChange={(e) => handleCashFlowChange(index, e.target.value)}
+            fullWidth
+          />
+          <IconButton
+            aria-label="delete"
+            color="error"
+            onClick={() =>
+              setCashFlows(cashFlows.filter((_, i) => i !== index))
+            }
+            sx={{ ml: 1 }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </Box>
+      ))}
+
+      <Box mt={2} display="flex" justifyContent="space-between">
+        <Button
+          variant="outlined"
+          startIcon={<AddCircleOutlineIcon />}
+          onClick={addCashFlow}
+        >
+          Agregar Flujo de Efectivo
+        </Button>
+        <Button variant="contained" color="primary" onClick={calculateIRR}>
+          Calcular TIR
+        </Button>
+        <Button variant="outlined" color="secondary" onClick={clearInputs}>
+          Limpiar
+        </Button>
+      </Box>
+
+      {irr !== null && (
+        <Box mt={3}>
+          <Typography variant="h6" color="primary">
+            TIR: {irr.toFixed(2)}%
+          </Typography>
+        </Box>
+      )}
+    </Container>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,36 +1,152 @@
-// src/pages/Home.tsx
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import styles from './styles.module.css';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Switch,
+  IconButton,
+  CssBaseline,
+  ThemeProvider,
+  Box,
+  Container,
+  List,
+  ListItem,
+  ListItemText
+} from '@mui/material';
+import { Brightness4, Brightness7 } from '@mui/icons-material';
+import { createTheme } from '@mui/material/styles';
 
 export const Home: React.FC = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Define el tema claro y oscuro con los colores de tu preferencia
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#a93226',
+      },
+      background: {
+        default: darkMode ? '#000000' : '#E0E0E0',
+      },
+    },
+    typography: {
+      fontFamily: 'Lato, sans-serif', // Fuente principal para el texto general
+      h1: {
+        fontFamily: 'Bebas Neue, sans-serif', // Fuente personalizada para los encabezados
+      },
+      h2: {
+        fontFamily: 'Bebas Neue, sans-serif',
+      },
+      h6: {
+        fontFamily: 'Oswald,italic' // Fuente para los botones
+      },
+    }
+  });
+
+  // Alterna entre los modos oscuro y claro
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className={styles.container}>
-      <nav className={styles.navbar}>
-        <h1 className={styles.title}>Ingeniería Económica</h1>
-        <ul className={styles.navList}>
-          <li>
-            <Link to="/interes-simple" className={styles.navLink}>Interés Simple</Link>
-          </li>
-          <li>
-            <Link to="/interes-compuesto" className={styles.navLink}>Interés Compuesto</Link>
-          </li>
-          <li>
-            <Link to="/periodo-recuperacion" className={styles.navLink}>Periodo de Recuperación</Link>
-          </li>
-          <li>
-            <Link to="/VPN" className={styles.navLink}>Valor Presente Neto</Link>
-          </li>
-          <li>
-            <Link to="/VAE" className={styles.navLink}>Valor Anual Equivalente</Link>
-          </li>
-          <li>
-            <Link to="/TIR" className={styles.navLink}>Tasa Interna de Rendimiento</Link>
-          </li>
-        </ul>
-      </nav>
-      <p className={styles.description}>Selecciona un tema para empezar:</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static" color="primary">
+        <Toolbar sx={{ display: 'flex', flexWrap: 'wrap' }}>
+          <Typography variant="h1" component="div" sx={{ flexGrow: 1 }}>
+            Ingeniería Económica
+          </Typography>
+          <IconButton color="inherit" onClick={toggleDarkMode}>
+            {darkMode ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          <Switch checked={darkMode} onChange={toggleDarkMode} />
+          <Box display="flex" gap={2} ml={2} flexWrap="wrap">
+            {[ // Menú de navegación
+              { label: 'Interés Simple', path: 'interes-simple' },
+              { label: 'Interés Compuesto', path: 'interes-compuesto' },
+              { label: 'Periodo de Recuperación', path: 'periodo-recuperacion' },
+              { label: 'VPN', path: 'VPN' },
+              { label: 'VAE', path: 'VAE' },
+              { label: 'TIR', path: 'TIR' },
+            ].map((topic, index) => (
+              <Link key={index} to={`/${topic.path}`} style={{ textDecoration: 'none' }}>
+                <Typography
+                  variant="button"
+                  component="div"
+                  sx={{
+                    color: 'white',
+                    backgroundColor: '#2e4053',
+                    borderRadius: 1,
+                    px: 1.5,
+                    py: 0.5,
+                    '&:hover': {
+                      backgroundColor: '#ec7063',
+                    },
+                  }}
+                >
+                  {topic.label}
+                </Typography>
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Container sx={{ mt: 4, textAlign: 'center' }}>
+        <Typography variant='h2'component="p" sx={{ mb: 2 }}>
+          Que es la Ingenieria Economica?
+        </Typography>
+        <Typography variant="h5" component="p" sx={{ mb: 2 }}>
+          La ingeniería económica es una disciplina que combina conocimientos de ingeniería con principios de microeconomía para tomar decisiones económicas. Su objetivo es evaluar alternativas de solución a problemas de ingeniería, y así ayudar a aumentar el valor de una empresa.
+        </Typography>
+        <Typography variant='h2'component="p" sx={{ mb: 2 }}>
+          Interés Simple
+        </Typography>
+        <Typography variant="h5" component="p" sx={{ mb: 2 }}>
+        El interés simple es el beneficio o interés que se obtiene de una inversión o de un préstamo, y se calcula sobre el capital inicial sin incluir los intereses acumulados anteriormente.
+        </Typography>
+        <Typography variant='h2'component="p" sx={{ mb: 2 }}>
+          Interés Compuesto
+        </Typography>
+        <Typography variant="h5" component="p" sx={{ mb: 2 }}>
+        El interés generado durante cada periodo de interés se calcula sobre el capital más el monto total del interés acumulado en todos los periodos anteriores. Así, el interés compuesto es un interés sobre el interés.        </Typography>
+        <Typography variant='h2'component="p" sx={{ mb: 2 }}>
+          Periodo de recuperacion de la inversion 
+        </Typography>
+        <Typography variant="h5" component="p" sx={{ mb: 2 }}>
+        El periodo de recuperación de la inversión (PRI), también conocido como payback, es el tiempo que tarda una empresa en recuperar el dinero invertido en un proyecto. Es una métrica fundamental para determinar si un proyecto es rentable o no.        </Typography>
+        <Typography variant='h2'component="p" sx={{ mb: 2 }}>
+          Valor Prensente Neto(VPN)
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText primary="Es un método que permite determinar si una inversión será rentable o no" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Se calcula restando el costo inicial de la inversión del valor actual de los flujos de efectivo futuros" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Si el VPN es positivo, el proyecto se considera rentable" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="El VPN es una fórmula matemática que se basa en el concepto del valor del dinero en el tiempo. Se utiliza para evaluar la rentabilidad de las inversiones y proyectos, y para elaborar presupuestos de capital." />
+          </ListItem>
+        </List>   
+        <Typography variant='h2'component="p" sx={{ mb: 2 }}>
+          Valor Anual Equivalente
+        </Typography>
+        <Typography variant="h5" component="p" sx={{ mb: 2 }}>
+        El valor anual equivalente (VAE) es un criterio de decisión para evaluar proyectos. En el contexto de la evaluación de proyectos de inversión, el VAE es la anualidad que tiene un valor presente igual al valor presente neto del proyecto.  
+        </Typography>
+        <Typography variant='h2'component="p" sx={{ mb: 2 }}>
+          Tasa Interna de Rendimiento
+        </Typography>
+        <Typography variant="h5" component="p" sx={{ mb: 2 }}>
+        La Tasa Interna de Retorno (TIR) es un indicador financiero que permite evaluar la rentabilidad de una inversión. Se trata de la tasa de descuento que hace que el valor presente neto (VAN) de los flujos de caja futuros de una inversión sea igual a su coste inicial. 
+        </Typography>
+      </Container>
+    </ThemeProvider>
   );
 };
